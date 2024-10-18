f2689f18d200efe099bf26ff230a74ef78c6eb42
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-10-18 15:00:52 +0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Login from "./features/Auth/components/Login";
 import Signup from "./features/Auth/components/Signup";
 import Sidebar from "./components/Sidebar";
 import UserManagement from "./features/UserManagement/components/UserManagement";
-import Products from "./features/Products/components/Products";
 import Settings from "./views/Settings/components/Settings";
+import Bids from "./features/Bids/components/Bids";
 import BidPurchaseOrders from "./features/BidPurchaseOrders/components/BidPurchaseOrders";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./components/NotFound";
@@ -23,9 +23,10 @@ const App = () => {
 
         <Route element={<ProtectedRoute />}>
           <Route path="user-management" element={<UserManagement />} />
-          <Route path="products" element={<Products />} />
-          <Route path="settings" element={<Settings />} />
+          yy <Route path="settings" element={<Settings />} />
           <Route path="sidebar" element={<Sidebar />} />
+          <Route path="bid-purchase-orders" element={<BidPurchaseOrders />} />
+          <Route path="bids" element={<Bids />} />
           <Route path="*" element={<NotFound />} />
         </Route>
 
