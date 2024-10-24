b629cab89ccaadddeabdd4c1bb8bfa38eae778d4
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-10-24 16:49:06 +0300

    fast forwarding

--- src/features/TaskManagement/components/Kanban/components/Kanban.jsx
+++ src/features/TaskManagement/components/Kanban/components/Kanban.jsx
@@ -6,10 +6,12 @@ const Kanban = () => {
   return (
     <div className="flex">
       <>
-        <Sidebar />
-        <div className="h-screen w-full bg-neutral-900 text-neutral-50">
-          <Board />
-        </div>
+        <>
+          <Sidebar />
+          <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+            <Board />
+          </div>
+        </>
       </>
     </div>
   );
