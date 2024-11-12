b920e9ee9d8c43d28af953f7f8a1a0d5216fb46c
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-11-12 10:54:22 +0300

    baseURL for deployment

--- src/features/baseQuery.js
+++ src/features/baseQuery.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8001/api/v1/",
+  baseUrl: "http://4.212.8.161:8001/api/v1/",
   credentials: "include",
 });
 
