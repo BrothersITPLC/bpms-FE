5848772f489f5917fb75fc9980c3326322947ec0
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-12-10 09:50:38 +0300

    dockerized frontend

--- src/features/baseQuery.js
+++ src/features/baseQuery.js
@@ -1,7 +1,10 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8001/api/v1/",
+  // baseUrl: "http://localhost:8001/api/v1/",
+  baseUrl:
+    "https://bpms-django.internal.wittydesert-75f5c753.francecentral.azurecontainerapps.io:8001/api/v1/",
+
   credentials: "include",
 });
 
