5f706663c901b6aa1a6b090ecfc08eea820b0761
Bit-dagim <dagim.asrat@brothersitplc.com>
2024-12-16 10:12:01 +0300

    base url

--- src/features/baseQuery.js
+++ src/features/baseQuery.js
@@ -1,8 +1,8 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "http://localhost:8001/api/v1/",
-  baseUrl: "/api/v1/",
+  baseUrl: "http://localhost:8001/api/v1/",
+  // baseUrl: "/api/v1/",
   // baseUrl:
   //   "https://bpms-backend.wittydesert-75f5c753.francecentral.azurecontainerapps.io:8001/api/v1/",
   credentials: "include",
